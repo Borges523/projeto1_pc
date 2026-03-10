03bbc6584e3e1af9a776795547eaaba092e34ed2
Borges523 <jvinifreitas@gmail.com>
2026-03-10 17:14:19 -0300

    Update Pyinvest.py
    projeto 1 - atualização de códigos corrigidos

--- Pyinvest.py
+++ Pyinvest.py
@@ -15,15 +15,15 @@ perc_lci = float(input('Percentual do LCI (%)')) / 100
 taxa_fii = float(input('Rentabilidade mensal FII (%)')) / 100
 meta = float(input('Meta financeira (R$)'))
 
-cdi_mensal = math.pow(1+cdi_anual, 1/12)- 1
+cdi_mensal = math.pow((1+cdi_anual), 1/12)-1
 #TOTAL INVESTIDO
-total_investido = capital + (aporte + meses)
+total_investido = capital + (aporte * meses)
 
 #CDB
-taxa_cdb = cdi_mensal * perc_cdb
-montante_cdb = (capital * math.pow((1+ taxa_cdb), meses) + (aporte * meses))
-lucro_cdb = montante_cdb - total_investido
-montante_cdb_liquido = total_investido + (lucro_cdb * 0.85)
+taxa_cdb=cdi_mensal*perc_cdb
+montante_cdb =(capital*math.pow((1+taxa_cdb), meses)+(aporte*meses))
+lucro_cdb=montante_cdb-total_investido
+montante_cdb_liquido=total_investido + (lucro_cdb * 0.85)
 
 #LCI
 taxa_lci = cdi_mensal * perc_lci
@@ -34,7 +34,7 @@ taxa_poupanca = 0.005
 montante_poupanca = (capital * math.pow((1+taxa_poupanca), meses)+ (aporte * meses))
 
 #FII - SIMULAÇOES
-montante_fii = (capital * math.pow((1+ taxa_fii),meses)+ (aporte * meses))
+montante_fii = (capital*math.pow((1+ taxa_fii),meses)+ (aporte * meses))
 vari1 = random.uniform(-0.03, 0.03)
 vari2 = random.uniform(-0.03, 0.03)
 vari3 = random.uniform(-0.03, 0.03)
@@ -49,7 +49,7 @@ simu5 = montante_fii + (montante_fii * vari5)
 
 media_fii = st.mean((simu1, simu2, simu3, simu4, simu5))
 mediana_fii = st.median((simu1, simu2, simu3, simu4, simu5))
-desvio_padrao = st.stdev((simu1, simu2, simu3, simu4, simu5))
+dp_fii = st.stdev((simu1, simu2, simu3, simu4, simu5))
 
 #FORMATAÇÃO MONETARIA
 data_simulacao = datetime.date.today()
@@ -68,8 +68,9 @@ print(f'LCI:{locale.currency(montante_lci, grouping=True)}')
 print(f'poupanca:{locale.currency(montante_poupanca, grouping=True)}')
 print(f'FII(media:{locale.currency(media_fii, grouping=True)}')
 print(f'mediana:{locale.currency(mediana_fii, grouping=True)}')
-print(f'desvio_padrao:{locale.currency(desvio_padrao, grouping=True)}')
+print(f'Desvio_padrao:{locale.currency(dp_fii, grouping=True)}')
 print(f'Meta atingida?:{media_fii >= meta}')
+
 
 
 
